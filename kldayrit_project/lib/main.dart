106045a9d0c3796e343cfb57156a4ad5bd01f69b
kldayrit <kldayrit@up.edu.ph>
2022-06-04 16:08:04 +0800

    Remaked Entire Home Page
    - Shows instead the Login Page at the start
    - Small button for registration under Login

--- kldayrit_project/lib/main.dart
+++ kldayrit_project/lib/main.dart
@@ -1,49 +1,131 @@
 import 'package:flutter/material.dart';
+import 'register_page.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Persistence';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      theme: ThemeData(primarySwatch: Colors.teal),
-      home: const MyStatefulWidget(title: 'To Do List'),
+      title: 'Start Page',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Login or Register')),
+        body: const MyStatefulWidget(),
+      ),
     );
   }
 }
 
 class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  //create a counter variable for Task id
-  // the one that will hold the text boxes and the buttons
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final bool _validate = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () {}, child: const Text('LOGiN')),
-            ElevatedButton(onPressed: () {}, child: const Text('REGiSTER'))
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Gumana ka',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                )),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+                padding: const EdgeInsets.all(10),
+                child: buildTextField('Enter Username', usernameController)),
+            Container(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: buildTextField('Enter Password', passwordController)),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                child: const Text('REGISTER'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                    usernameController.clear();
+                    passwordController.clear();
+                  }
+                },
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Do not have an Account? Cry or'),
+                TextButton(
+                  child: const Text(
+                    'REGISTER',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ShowTasksPage()));
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildTextField(String label, TextEditingController _controller) {
+    return TextFormField(
+      controller: _controller,
+      decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          errorBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 213, 82, 125))),
+          focusedErrorBorder: const OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 234, 146, 175))),
+          labelText: label,
+          labelStyle: const TextStyle(color: Color.fromARGB(255, 11, 69, 169)),
+          floatingLabelStyle:
+              const TextStyle(color: Color.fromARGB(255, 90, 113, 241)),
+          errorText: _validate ? 'Value can\'t be empty' : null),
+      style: const TextStyle(color: Colors.indigo),
+      validator: (value) {
+        //validates if value in controller/textfield is not empty
+        if (value == null || value.isEmpty) {
+          return 'Please $label';
+        }
+
+        return null;
+      },
     );
   }
 }
